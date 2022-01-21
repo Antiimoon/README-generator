44ae0e660f34623febdb5a431d67661d8fd209b5
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-01-20 18:38:15 -0800

    save any updates

--- Develop/index.js
+++ Develop/index.js
@@ -73,7 +73,7 @@ function init() {
     inquirer.prompt(questions)
         .then(function(data) {
             console.log(data);
-            writeToFile("readme-guide.md", generateMarkdown(data));
+            writeToFile("README.md", generateMarkdown(data));
         });
 }
 
